d959f2b23cc78a98e0c36c34d7ca51f758fc713c
monira-jahan-tania <tania598330@gmail.com>
2022-03-25 22:47:57 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className='title'>
+        <h1>Women's World</h1>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
